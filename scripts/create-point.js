e3b3202cc15eac194746267172e55865b7b5692e
WellintonCamboim <wellinton.camboim@gmail.com>
2020-06-10 22:43:58 -0300

    Ítens de coleta (html e css)

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -34,7 +34,7 @@ function getCities(event) {
         .then(res => res.json())
         .then(cities => {
             for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false
@@ -45,3 +45,22 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+//Itens de coleta
+//Pegar todos os li's
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    //adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = event.target.dataset.id
+
+}
+
