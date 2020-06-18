de3c19d3963eb0e28bfa6f3010ae40f344a25585
WellintonCamboim <wellinton.camboim@gmail.com>
2020-06-17 21:09:14 -0300

    config server and routes

--- src/server.js
+++ src/server.js
@@ -2,4 +2,19 @@ const express = require("express")
 
 const server = express()
 
+//configurar pasta pública
+server.use(express.static("public"))
+
+//configurar caminhos(rotas) da minha aplicação
+//home
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+//create-point
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+//start server
 server.listen(3000)
